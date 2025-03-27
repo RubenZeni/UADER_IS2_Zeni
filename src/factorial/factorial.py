e32f5c638c32fa904d37378be60de7738c8362ec
RubenZeni <zenirubendario@gmail.com>
2025-03-26 22:06:01 -0300

    factorial.py ahora solicita el número si se omite

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -20,9 +20,9 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
+if len(sys.argv) < 2:
+    num = int(input("Ingrese un número: "))
+else:
+    num=int(sys.argv[1])
 print("Factorial ",num,"! es ", factorial(num)) 
 
